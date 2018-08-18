2fbadac8c3901a5aeb7e381422cbbf25490b7929
唐鑫涛 <792883583@qq.com>
2018-08-18 22:18:13 +0800

    完成列表显示，等待开发播放界面

--- src/view/music_footer/index.vue
+++ src/view/music_footer/index.vue
@@ -3,8 +3,19 @@
     <div class="music_icon_left">
       <p></p>
     </div>
-    <div class="music_content">
-      {{$t('projectName')}}
+    <div style="margin:auto;width:1300px;height:100%;">
+      <div class="music_content">
+        {{$t('projectName')}}
+      </div>
+      <div class="icon_music_list">
+        <div class="music_list">
+          <div class="music_body">
+            <ul>
+              <li v-for="item in music_list" :key="item.id">{{item.name+"   歌唱者："+item.singer}}</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -15,18 +26,23 @@ import axios from 'axios'
 export default {
   data() {
     return {
-      music_width:0
+      music_width:0,
+      music_list:[]
     };
   },
   created(){
     this.music_width=`${document.body.clientWidth}px`;
-    util.ajax.get('/static/data.json')
+    axios.get('/static/data.json')
     .then(rep => {
-      console.log(rep.data);
+      this.music_list=rep.data.music.all;
     })
+  },
+  mounted(){
+    
   }
 };
 </script>
 <style lang='less' scoped>
 @import './index.less';
+@import '../public/public.less';
 </style>
