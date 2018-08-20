6fb4eb7297b891fdfe954f79deceda6c7ec0a0f4
唐鑫涛 <792883583@qq.com>
2018-08-20 23:29:07 +0800

    完成接口对接。搜索等功能

--- src/view/music_footer/index.vue
+++ src/view/music_footer/index.vue
@@ -3,14 +3,17 @@
     <div class="music_icon_left">
       <p></p>
     </div>
-    <Button type="primary" @click="clearlocal(1)">退出！</Button>
     <div style="margin:auto;width:1300px;height:100%;" v-if="state">
       <div class="music_content">
         <div class="musci_title_img">
           <img :src="img_title" :alt="img_alt" width="70px">
         </div>
         <div class="music_jindu">
-          <aplayer autoplay :music="music_object" :showLrc="true"/>
+          <aplayer autoplay="autoplay" :music="music_object" :showLrc="true" v-if="jiuxu" @ended="music_end"/>
+          <Spin fix v-show="!jiuxu">
+            <Icon type="ios-loading" size=18 class="demo-spin-icon-load"></Icon>
+            <div>加载中...</div>
+          </Spin>
         </div>
         <div class="music_gongneng"></div>
       </div>
@@ -18,18 +21,26 @@
         <div class="music_list">
           <div class="music_body">
             <div class="music_title">
-              <div class="music_show" v-if="title_show">
+              <div class="music_show" v-show="title_show">
                 <p>歌曲列表</p>
-                <p @click="search"><img src="../../assets/images/index_02.png"></p>
+                <p @click="title_show=!title_show"><img src="../../assets/images/index_02.png" width="20px;" style="vertical-align:middle;cursor:pointer;"></p>
               </div>
-              <div class="music_search" v-else>
-                <Input v-model="music_search" placeholder="请输入您要搜索的音乐 / 专辑 / 歌手 / 歌单 / 用户" style="width: 80%"/>
-                <Button type="primary" shape="circle" icon="ios-search" style="widtd:20%" @click="on_search">搜索</Button>
+              <div class="music_search" v-show="!title_show">
+                <Input v-model="music_search" placeholder="请输入您要搜索的音乐 / 专辑 / 歌手 / 歌单 / 用户" style="width: 75%;float:left;"/>
+                <Button type="primary" shape="circle" icon="ios-search" style="width:20%;float:right;" @click="on_search">搜索</Button>
               </div>
             </div>
-            <ul>
-              <li v-for="item in music_list" :key="item.id">
-                <div @dblclick="on_diange(item)" style="cursor:pointer;" :class="jishiqi==item.id?'on_zhengzaibf':''">
+            <ul v-show="title_show">
+              <li v-for="item in music_data" :key="item.id" onselectstart ='return false'>
+                <div @dblclick="on_diange(item.id)" style="cursor:pointer;" :class="jishiqi==item.id?'on_zhengzaibf':''">
+                  <div class="li_left_margin">{{item.name}}</div>
+                  <div class="li_content_bigmargin">{{item.singer}}</div>
+                </div>
+              </li>
+            </ul>
+            <ul class="search_list" v-show="!title_show">
+              <li v-for="item in search_data" :key="item.id" onselectstart ='return false'>
+                <div @dblclick="on_diange(item.id)" style="cursor:pointer;" :class="jishiqi==item.id?'on_zhengzaibf':''">
                   <div class="li_left_margin">{{item.name}}</div>
                   <div class="li_content_bigmargin">{{item.singer}}</div>
                 </div>
@@ -66,6 +77,7 @@ export default {
   data() {
     return {
       music_width:0,
+      jiuxu:false,
       userid:0,
       formLeft:{
         username:'',
@@ -82,6 +94,9 @@ export default {
         lrc:''
       },
       music_url:'',
+      page_index:30,
+      music_id:0,
+      music_data:[],
       jishiqi:1,
       img_title:'',
       img_alt:'',
@@ -96,73 +111,38 @@ export default {
         userpassword: [
           { required: true, message: '请填写密码', trigger: 'blur' }
         ]
-      }
+      },
+      search_data:[]
     };
   },
   created(){
     this.music_width=`${document.body.clientWidth}px`;
-    // axios.get('http://localhost:3000/lyric?id=347230')
-    // .then(rep => {
-    //   console.log(rep.data);
-    //   // this.music_list=rep.data.music.all;
-    //   // this.jishiqi= Math.floor(Math.random()*(1 - 356) + 356);
-    //   // this.music_url=this.music_list[this.jishiqi].url;
-    //   // this.img_alt=this.music_list[this.jishiqi].type;
-    //   // this.img_title=this.music_list[this.jishiqi].img_url;
-    // })
     if(this.userid!=0||localStorage.getItem('userid')!=null){
       axios.get('http://localhost:3000/user/playlist?uid='+localStorage.getItem('userid')+'')
       .then(rep=>{
-        console.log(rep.data);
         this.state=true;
+      })
+      axios.get('http://localhost:3000/personalized/newsong')
+      .then(rep=>{
+        var data=rep.data.result;
+        for(var i=0;i<data.length;i++){
+          this.music_data.push({
+            name:data[i].name,
+            id:data[i].id,
+            singer:data[i].song.artists.length==1?data[i].song.artists[0].name:data[i].song.artists[0].name+"-"+data[i].song.artists[1].name
+          })
+          if(i==0){
+            this.on_diange(this.music_data[0].id)
+          }
+        }
       })
     }
     let that=this;
     setTimeout(function(){
       that.clearlocal();
     },10800000);
-    this.music_object.title='secret base~君がくれたもの~'
-    this.music_object.artist='Silent Siren'
-    this.music_object.src='https://moeplayer.b0.upaiyun.com/aplayer/secretbase.mp3'
-    this.music_object.pic='https://moeplayer.b0.upaiyun.com/aplayer/secretbase.jpg'
-    this.music_object.lrc='http://localhost:3000/lyric?id=33894312'
   },
   methods:{
-    on_click_music(type){
-      if(type==1){
-        if(this.music_list.length<this.jishiqi){
-          util.error("最后一首！");
-          return;
-        }else{
-          this.jishiqi++
-          this.music_url=this.music_list[this.jishiqi].url;
-          this.img_alt=this.music_list[this.jishiqi].type;
-          this.img_title=this.music_list[this.jishiqi].img_url;
-          this.if_time();
-        }
-      }else{
-        if(this.jishiqi==0){
-          util.error("第一首！");
-          return;
-        }else{
-          this.jishiqi--
-          this.music_url=this.music_list[this.jishiqi].url;
-          this.img_alt=this.music_list[this.jishiqi].type;
-          this.img_title=this.music_list[this.jishiqi].img_url;
-          this.if_time();
-        }
-      }
-    },
-    on_bofang(item){
-      
-      
-    },
-    search(){
-      this.title_show=false;
-    },
-    time_kaishi(){
-      
-    },
     if_time(){
       
     },
@@ -173,29 +153,51 @@ export default {
       this.dangqian_time=type.currentTime;
     },
     on_diange(item){
-      this.music_url=item.url;
-      this.img_alt=item.type;
-      this.img_title=item.img_url;
+      this.jiuxu=false;
+      this.music_id=item;
+      axios.get('http://localhost:3000/music/url?id='+item+'')
+      .then(rep=>{
+        var data=rep.data;
+        if(data.data[0].url!=""&&data.data[0].url!=null&&data.data[0].url!=undefined){
+          this.music_object.src=data.data[0].url;
+          axios.get('http://localhost:3000/song/detail?ids='+item+'')
+          .then(rep=>{
+            var data=rep.data.songs;
+            this.music_object.artist=data[0].ar.length==1?this.music_object.artist=data[0].ar[0].name:data[0].ar[0].name+"-"+data[0].ar[1].name
+            this.music_object.title=data[0].name;
+            this.music_object.pic=data[0].al.picUrl;
+            axios.get('http://localhost:3000/lyric?id='+item+'')
+            .then(rep=>{
+              var data=rep.data;
+              this.music_object.lrc="";
+              this.music_object.lrc=data.lrc.lyric;
+              this.jiuxu=true;
+            })
+          })
+        }else{
+          util.error("对不起！您点播的歌曲暂无权限和资料，请换歌！");
+          this.on_diange(this.music_data[0].id);
+          this.jiuxu=true;
+          return;
+        }
+      })
       this.jishiqi=0;
-      this.jishiqi=this.jishiqi+parseInt(item.id)
+      this.jishiqi=this.jishiqi+parseInt(item)
     },
     deLogin(){
       axios.get('http://localhost:3000/login/cellphone?phone='+this.formLeft.username+'&password='+this.formLeft.userpassword+'')
       .then(rep=>{
         let data=rep.data
-        console.log(data);
         this.state=true;
         this.userid=data.account.id;
         localStorage.setItem('userid',this.userid);
         axios.get('http://localhost:3000/user/playlist?uid='+this.userid+'')
         .then(rep=>{
-          console.log(rep.data);
           this.zhuangtai();
         })
       })
     },
     zhuangtai(){
-      console.log(localStorage.getItem('userid'));
       if(localStorage.getItem('userid')!=""){
         axios.get('http://localhost:3000/login/refresh')
         .then(rep=>{
@@ -211,7 +213,6 @@ export default {
         util.success("退出成功！请重新登录！");
         return
       }else if(document.hidden==true&&(document.visibilityState=='hidden'||document.visibilityState=='prerender')){
-        console.log("asdadasdasdasds");
         util.error("您的登录状态已过期，请重新登录！");
         localStorage.removeItem('userid');
         this.state=false;
@@ -219,10 +220,34 @@ export default {
       }
     },
     on_search(){
-      axios.get('http://localhost:3000/search?keywords='+this.music_search+'')
+      // .splice(1,1)
+      axios.get('http://localhost:3000/search?keywords='+this.music_search+'?limit='+this.page_index)
       .then(rep=>{
-        console.log(rep.data);
+        var data=rep.data.result.songs;
+        console.log(data);
+        this.search_data=[];
+        for(var i=0;i<data.length;i++){
+          this.search_data.push({
+            name:data[i].name,
+            id:data[i].id,
+            singer:data[i].artists.length==1?data[i].artists[0].name:data[i].artists[0].name+"-"+data[i].artists[1].name
+          });
+        }
+        console.log(this.search_data);
       })
+    },
+    music_end(){
+      for(var a=1;a<this.music_data.length;a++){
+        if(this.music_data[a].id==this.music_id){
+          if(a+1<this.music_data.length){
+            this.on_diange(this.music_data[a+1].id)
+            return;
+          }else{
+            this.on_diange(this.music_data[0].id)
+            return;
+          }
+        }
+      }
     }
   },
   watch:{
